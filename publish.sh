22980f290b99ef939c356c703371d2ebe56394e5
odin thomas rochmann <odin.rochmann@gmail.com>
2019-11-03 23:36:23 +0100

    mics

--- publish.sh
+++ publish.sh
@@ -3,5 +3,5 @@ BRANCH=$(git rev-parse --abbrev-ref HEAD)
 if [ $BRANCH == "master" ]; then
     lerna publish
 else
-    lerna publish --canary --dist-tag $BRANCH
+    lerna publish --no-git-reset --canary --dist-tag $BRANCH
 fi
